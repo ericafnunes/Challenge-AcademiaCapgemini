7a9f36c0132b4fcf6a2ff86de56a64ad749363b1
ericafnunes <you@example.com>
2022-02-16 20:14:32 -0300

    questao 1 do desafio resolvida com Java

---
+++ questao1/question_1/src/App.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        Scanner entrada = new Scanner(System.in);
+       
+        ArrayList<String> degraus = new ArrayList();
+
+        System.out.print("Digite a quantidade de vezes: ");
+        int qtdDegraus = entrada.nextInt();
+
+        for (int i = 0; i < qtdDegraus; i++) {
+            degraus.add(" ".repeat(qtdDegraus - i) + "*".repeat(i + 1));
+        }
+
+        for (String d : degraus) {
+            System.out.println(d);
+        }
+    }
+}
